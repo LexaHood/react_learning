b71d8d10c58ee1fea0349262f6918d9fd51456d1
Alexey Petrov <alexey@Lexa-PC.localdomain>
2022-09-07 21:47:38 +0300

    Подключение css файла

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,3 +1,4 @@
+import './Car.css';
 // function Car() {
 //   return (
 //     <h3>This is car compomemt</h3>
@@ -25,14 +26,7 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => (
-  <div style={{
-    border: '1px solid #ccc',
-    marginBottom: '10px',
-    display: 'inline-block',
-    padding: '10px',
-    boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
-    borderRadius: '5px'
-  }}>
+  <div className="Car">
     <p>Car name: <strong>{props.name}</strong></p>
     <p>Year: <strong>{props.year}</strong></p>
     <input type={'text'} onChange={props.onChangeName} value={props.name}></input>
